4c041fde55d5d443a960e46ef2cf36fd74c92c63
Chris Walker <cmw161@uclive.ac.nz>
2018-04-02 00:52:32 +1200

    Fixity fix fix fix

--- auto-wait.user.js
+++ auto-wait.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TagPro Auto Wait
-// @version      2.3
+// @version      2.4.0
 // @description  Wait for a 4 or 6 player TagPro game. Be alerted when it begins. Anti-AFK kick.
 // @author       Zagd
 // @downloadURL  https://github.com/zagd/tagpro-scripts/blob/master/auto-wait.user.js
@@ -25,8 +25,11 @@ How to use:
     If there are between 1 and 3 players, send a message in chat saying "!wait 4".
     If there are 4 or 5 players, send a message in chat saying "!wait 6".
     By the default, the notification that the target player count has been reached is an alarm noise and "GO!".
+    ////// EPILEPSY WARNING \\\\\\
     If you would like more notification than this, use either "!wait 4 alert" or "!wait 6 alert".
-    With this option, you will get the sound notifications, and a visual browser alert, which will focus your game tab.
+    With this option, you will get the sound notifications, and a visual browser alert, which will flash the
+    screen red and blue, and flash the browser tab's title.
+    \\\\\\ EPILEPSY WARNING //////
 
 Disclaimer:
     If you fail to recognize the notifications, and get reported / kicked for AFK, I am not to blame.
@@ -41,20 +44,25 @@ Disclaimer:
 */
 
 var waiting = GM_getValue("isWaiting");
+var visual_alert = false;
+document.onclick= function() {
+    visual_alert = false;
+};
 
 tagpro.ready(function() {
-    tagpro.socket.on('map', function (map) {
+    tagpro.socket.on("map", function (map) {
         waiting = GM_getValue("isWaiting");
         if (waiting) {
             insertHTML(4);
-            wait(4, true);
+            visual_alert = true;
+            wait(4);
         }
         tagpro.socket.on("chat", function (data) {
             if (!data || data === undefined || data === null) return;
             if (waiting) return;
-            if (data.from === tagpro.playerId && /^!wait 4 */.test(data.message)) {
+            if (data.from === tagpro.playerId && /^!wait 4 ?(alert)?$/.test(data.message)) {
                 setUpWait(4, data);
-            } else if (data.from === tagpro.playerId && /^!wait 6 */.test(data.message)) {
+            } else if (data.from === tagpro.playerId && /^!wait 6 ?(alert)?$/.test(data.message)) {
                 var playerCount = getPlayerCount();
                 if (playerCount >= 4) {
                     setUpWait(6, data);
@@ -75,26 +83,68 @@ function setUpWait(target, data) {
         insertHTML(target);
         waiting = 1;
         GM_setValue("isWaiting", waiting);
-        var alert = (/alert$/.test(data.message));
-        wait(target, alert);
+        visual_alert = (/^!wait (4|6) alert$/.test(data.message));
+        wait(target);
     }
 }
 
-function wait(target, alert) {
-    console.log('.');
+function wait(target) {
     var playerCount = getPlayerCount();
     if (playerCount >= target) {
         new Audio("http://tagpro-diameter.koalabeast.com/sounds/alertlong.mp3").play();
         new Audio("http://tagpro-diameter.koalabeast.com/sounds/go.mp3").play();
-        if (alert) {
-            alert("TagPro game reached player count target!");
+        if (visual_alert) {
+            flash_alert();
         }
         unWait();
     } else if (waiting) {
         antiAFK();
-        setTimeout(function() { wait(target, alert); }, 2500);
+        setTimeout(function() { wait(target); }, 2500);
+    }
+}
+
+function flash_alert() {
+    var css = "#flashAlert {" +
+        "position: fixed;" +
+        "z-index: 9999999;" +
+        "width: 100%;" +
+        "height: 100%; " +
+        "font-size: 64px;" +
+        "color: white;" +
+        "text-shadow:" +
+        "-1px -1px 0 #000," +
+        "1px -1px 0 #000," +
+        "-1px 1px 0 #000," +
+        "1px 1px 0 #000;" +
+        "text-align: center;" +
+        "vertical-align: middle;" +
+        "}";
+    var style = document.createElement("style");
+
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
     } else {
-        unWait();
+        style.appendChild(document.createTextNode(css));
+    }
+    document.getElementsByTagName("head")[0].appendChild(style);
+    window.document.body.insertAdjacentHTML("beforeend", "<div id='flashAlert'>Click anywhere</div>");
+    var flashAlert = document.getElementById("flashAlert");
+    var old_title = document.title;
+    _flash_alert(1);
+    function _flash_alert(count) {
+        if (visual_alert) {
+            if (count % 2 === 0) {
+                document.title = "TagPro !!!!!";
+                flashAlert.style.backgroundColor = "rgba(255,0,0,0.15)";
+            } else {
+                document.title = "!!!!! TagPro";
+                flashAlert.style.backgroundColor = "rgba(0,0,255,0.15)";
+            }
+            setTimeout(function () { _flash_alert(count + 1); }, 500);
+        } else {
+            flashAlert.parentNode.removeChild(flashAlert);
+            document.title = old_title;
+        }
     }
 }
 
@@ -117,8 +167,8 @@ function getPlayerCount() {
 
 function antiAFK() {
     if (tagpro.state === 1) {
-        tagpro.socket.emit('keydown', {k: 'space'});
-        tagpro.socket.emit('keyup', {k: 'space'});
+        tagpro.socket.emit("keydown", {k: "space"});
+        tagpro.socket.emit("keyup", {k: "space"});
     }
 }
 
@@ -129,42 +179,36 @@ function chatToAll(message) {
 }
 
 function insertHTML(target) {
-    var css = '#waitWarning {' +
-        'font-size: 80% ;' +
-        'color: #00FF00;' +
-        'top: 10px;' +
-        'left: 100px;' +
-        'position: absolute;' +
-        '}';
-    css = css.concat(
-        '#unWaitButton {' +
-        'border: none;' +
-        'margin-left: 15px;' +
-        'cursor: pointer;' +
-        'z-index: 999999;' +
-        'position: fixed;' +
-        '}'
+    window.document.getElementById("exit").insertAdjacentHTML(
+        "afterend",
+        "<div id='waitWarning'>" +
+        "You are waiting for " + String(target) + " or more players and will not be kicked for AFK." +
+        "<button id='unWaitButton'>Stop Waiting</button>" +
+        "</div>"
     );
-    var style = document.createElement('style');
-
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-    document.getElementsByTagName('head')[0].appendChild(style);
-
-    window.document.getElementById('exit').insertAdjacentHTML(
-        'afterend',
-        '<div id="waitWarning">' +
-        'You are waiting for ' + String(target) + ' or more players and will not be kicked for AFK.' +
-        '<button id="unWaitButton">Stop Waiting</button>' +
-        '</div>'
-    );
-    window.document.getElementById('unWaitButton').addEventListener('click', unWait);
+    window.document.getElementById("unWaitButton").addEventListener("click", unWait);
+    window.document.getElementById("waitWarning").style = window.document.getElementById("exit").style;
+    window.document.getElementById("waitWarning").style.position = "absolute";
+    window.document.getElementById("waitWarning").style.left = "100px";
 }
 
 function removeHTML() {
     var element = document.getElementById("waitWarning");
     element.parentNode.removeChild(element);
 }
+
+css = "#unWaitButton {" +
+    "border: none;" +
+    "margin-left: 15px;" +
+    "cursor: pointer;" +
+    "z-index: 999999;" +
+    "position: fixed;" +
+    "color: black;" +
+    "}";
+var style = document.createElement("style");
+if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+document.head.appendChild(style);
