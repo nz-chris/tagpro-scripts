db0b45af6b2713cd43f7d9c4ca3a96e745f82f56
Chris Walker <cmwalker.nz@gmail.com>
2020-04-23 20:23:48 +1200

    Build in backkground image changing and update includes

--- remove-black-background.user.js
+++ remove-black-background.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
-// @name         TagPro Remove Black Background
+// @name         TagPro Remove Black Background, Change Background Image
 // @version      1.0
-// @description  Remove the black background from the TagPro canvas. Made with love using stolen code from NewCompte (https://www.reddit.com/r/TagPro/comments/2xwwz4/userscript_newcomptes_transparent_canvas/) and nabby (https://gist.github.com/nabbynz/24c9c965b93e9f12660436d26c6e4ac1)
+// @description  Remove the black background from the TagPro canvas. Made with love using stolen code from NewCompte, nabby, and Catalyst.
 // @author       Zagd, but not really.
 // @include      *://tagpro.koalabeast.com/game
+// @include      *://tagpro.koalabeast.com/game?*
 // @include      *://*.newcompte.fr:*
 // @include      *://tagpro-maptest.koalabeast.com:*
 // ==/UserScript==
 
-// Fix the chat. Stolen from nabby.
+// If you want to change the background image. Stolen from Catalyst (https://gist.github.com/catalyst518/d424c4d1b1a2d8222bd02487c1339205/raw/Tagpro-Transparent-Canvas-and-Background.user.js).
+// Remove the "//" at the start of the next line, and you can change the imgur url to whatever you want.
+//$('html').css({'background-image':'url(https://i.imgur.com/RP4nVwp.png)'});
+
+// Fix the chat. Stolen from nabby (https://gist.github.com/nabbynz/24c9c965b93e9f12660436d26c6e4ac1).
 const resizeChat = function() {
     const viewportOffset = $("#viewport").offset();
     const viewportHeight = $("#viewport").height();
@@ -27,7 +32,7 @@ tagpro.ready(function () {
     tagpro.chat.resize = resizeChat;
     $("#chatHistory").hide();
 
-    // Remove the black background. Stolen from NewCompte.
+    // Remove the black background. Stolen from NewCompte (https://www.reddit.com/r/TagPro/comments/2xwwz4/userscript_newcomptes_transparent_canvas/).
     const oldCanvas = $(tagpro.renderer.canvas);
     oldCanvas.hide();
     const newCanvas = $('<canvas id="viewport" width="1280" height="800"></canvas>');
