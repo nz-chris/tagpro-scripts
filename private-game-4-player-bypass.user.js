078d637a11a4b8b66b99a14b560c974161c448fa
Chris Walker <cmwalker.nz@gmail.com>
2020-10-17 15:40:55 +1300

    go away

---
+++ private-game-4-player-bypass.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         TagPro Bypass Private Game 4 Player Minimum
+// @version      1.0.0
+// @author       Zagd
+// @downloadURL  https://github.com/zagd/tagpro-scripts/raw/master/private-game-4-player-bypass.user
+// @updateURL    https://github.com/zagd/tagpro-scripts/raw/master/private-game-4-player-bypass.user
+// @include      *://tagpro.koalabeast.com/groups/*
+// ==/UserScript==
+
+const launchButton = document.getElementById("launch-private-btn");
+
+let needMsgCount = 0;
+
+if (launchButton) {
+    const getNeedMsgsCount = () => {
+        const chatLog = document.querySelector(".chat-log");
+        const count = [...chatLog.querySelectorAll(".chat-message")]
+            .filter(msg => msg.innerText.match(/^Need [123] more players to launch private game\.$/))
+            .length;
+        return count;
+    };
+
+    const setNeedMsgsCount = () => {
+        needMsgCount = getNeedMsgsCount();
+    };
+
+    const onLaunch = () => {
+        if (getNeedMsgsCount() > needMsgCount) {
+            location.href = "https://tagpro.koalabeast.com/games/find";
+        }
+    };
+
+    launchButton.addEventListener("mousedown", setNeedMsgsCount);
+    launchButton.addEventListener("click", onLaunch);
+}
